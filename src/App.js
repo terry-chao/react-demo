f0fd11155a12128931a309cdabfc432b3bc8ca58
Terry <terrychao.me@gmail.com>
2024-03-07 15:52:23 +0800

    test: props

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import { useState } from "react"
-const count = 100
-function getName() {
-  return 'jack'
+
+function Son(props) {
+  console.log(props);
+  return <div>this is son</div>
 }
 
-const list = [
-  {id: 1001, name: "Vue"},
-  {id: 1002, name: "React"},
-  {id: 1003, name: "Angular"},
-]
-
-const login = false
-
 function App() {
-  const [count, setCount] = useState(0)
-  const clickHandler = (e) => {
-    setCount(count+1)
-    console.log("button 被按下", e);
-  }
-
-  const [form, setFrom] = useState({name: 'jack'})
-  const clickHandler2 = () => {
-    setFrom({
-      ...form, 
-      name: 'aaa'
-    })
-  }
-
+  const name = 1
   return (
     <div className="App">
-      <button onClick={clickHandler}>{count}</button>
-      <button onClick={clickHandler2}>{form.name}</button>
+      <Son name={name} />
     </div>
   );
 }
